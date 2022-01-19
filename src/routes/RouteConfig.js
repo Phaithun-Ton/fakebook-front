6c55f61c7e87c3df4b72caede04c1b8f38008c74
Phaithun-Ton <phaithun.ap@gmail.com>
2022-01-19 11:27:00 +0700

    config axios local storafe

--- src/routes/RouteConfig.js
+++ src/routes/RouteConfig.js
@@ -1,5 +1,9 @@
 import { Route, Routes } from "react-router-dom";
+import MainLayout from "../components/layouts/MainLayout";
+
 import PublicLayout from "../components/layouts/PubilcLayout";
+import Friend from "../pages/Friend";
+import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 
@@ -10,6 +14,10 @@ function RouteConfig() {
         <Route path="" element={<Login />} />
         <Route path="register" element={<Register />} />
       </Route>
+      {/* <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="friend" element={<Friend />} />
+      </Route> */}
     </Routes>
   );
 }
